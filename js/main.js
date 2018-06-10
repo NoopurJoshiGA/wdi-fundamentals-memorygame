5fe852944ab53107a6843a96eff919a6d90a5bd0
NoopurJoshiGA <noopur.joshi13@gmail.com>
2018-06-10 19:05:09 +0100

    created game logic

---
+++ js/main.js
@@ -0,0 +1,12 @@
+//check js is working
+alert("yes it's working");
+
+//declare 4 cards, 2 queens and 2 kings
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+//display two cards that are flipped in console
+console.log("User flipped " + cardTwo);
+console.log("User flipped " + cardFour);
