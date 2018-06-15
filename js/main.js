b91cd7779c1fd5f033a147a12b1ee1c41d7bd7f8
NoopurJoshiGA <noopur.joshi13@gmail.com>
2018-06-15 15:50:32 +0100

    unit 11 assignment added result text box

--- js/main.js
+++ js/main.js
@@ -25,19 +25,23 @@ var cards = [
 }
 ];
 
-//create empty array to hold the cards in play
+//create empty array to hold the cards selected
 var cardsInPlay = [];
 
 //function to check for a match
 function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
+		//alert("You found a match!");
+		document.getElementById('resultText').innerHTML = "You found a match!";
 	}
 
 	else {
-		alert("Sorry, try again.");
+		//alert("Sorry, try again.");
+		document.getElementById('resultText').innerHTML = "Sorry, try again.";
 	}
+	cardsInPlay = [];
 }
+
 
 //create flipCard function
 function flipCard() {
@@ -47,14 +51,16 @@ function flipCard() {
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 
-//push the flipped card to the cardsInPlay array
+//add the flipped card to the cardsInPlay array
 cardsInPlay.push(cards[cardId].rank);
 
 this.setAttribute('src', cards[cardId].cardImage);
+this.setAttribute("class", "cardSelected");
 
+//see if 2 cards have been played
 if(cardsInPlay.length === 2) {
 	checkForMatch();
-}
+	}
 }
 
 //create function to add images to the board
@@ -70,8 +76,20 @@ function createBoard() {
 
 createBoard();
 
+function btnReset() {
+	//remove the result text
+	//restore the selected cards to "images/back.png"
+	document.getElementById('resultText').innerHTML = "";
+	var x = document.getElementsByClassName("cardSelected");
+	for(i=0; i<x.length; i++) {
+		x[i].setAttribute('src', 'images/back.png');
+
+	}
+}
+
 //flipCard(0);
 //flipCard(2);
+
 
 
 
